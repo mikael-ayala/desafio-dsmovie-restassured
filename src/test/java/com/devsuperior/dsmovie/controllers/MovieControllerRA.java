3731728ef979e827f4d90b3bed7cc8547d84bfd4
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-05-29 17:15:02 -0300

    add imports and first test on MovieControllerRA

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,12 +1,41 @@
 package com.devsuperior.dsmovie.controllers;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import com.devsuperior.dsmovie.tests.TokenUtil;
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MovieControllerRA {
+
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private String clientToken, adminToken, invalidToken;
+	private String title;
+	private Long existingMovieId, nonExistingMovieId;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		baseURI = "http://localhost:8080";
+
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
+		adminUsername = "alex@gmail.com";
+		adminPassword = "123456";
+
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+		invalidToken = adminToken + "xpto";
+	}
 	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		given()
+			.get("/movies")
+		.then()
+			.statusCode(200);
 	}
 	
 	@Test
